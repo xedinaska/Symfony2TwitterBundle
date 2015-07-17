f09e151904b44fd2ff3761dacd41ff504af8adfc
Polevoy Alexander <xedinaska@gmail.com>
2015-07-17 17:47:39 +0300

    Dummy controller added

--- twitterAPP/src/Xedinaska/TwitterBundle/Controller/DefaultController.php
+++ twitterAPP/src/Xedinaska/TwitterBundle/Controller/DefaultController.php
@@ -3,29 +3,41 @@
 namespace Xedinaska\TwitterBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Xedinaska\TwitterBundle\Service\Encoder\BearerTokenGenerator;
+use Xedinaska\TwitterBundle\Service\TwitterRequestHandler;
+use Xedinaska\TwitterBundle\Service\TwitterResponseReader;
 
 class DefaultController extends Controller
 {
     public function indexAction($name)
     {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "https://api.twitter.com/1.1/followers/ids.json");
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        $output = curl_exec($ch);
-        curl_close($ch);
+        /*$url = "https://api.twitter.com/oauth2/token";
 
-        var_dump($output);
+        $requestHandler = new TwitterRequestHandler();
+        $output = $requestHandler->send($url, true);
 
-        if ($output)
-        {
-            $tweets = json_decode($output,true);
+        $responseReader = new TwitterResponseReader($output);
+        echo 'Code: ' . $responseReader->getCode();
+        echo '<br>Message: ' . $responseReader->getMessage();*/
 
-            foreach ($tweets as $tweet)
-            {
-                print_r($tweet);
-            }
-        }
+        $tokenGenerator = new BearerTokenGenerator();
+        $accessToken = $tokenGenerator
+            ->getAccessToken('47AdsMGL5UhWK9N1naQRQ', 'F1Hy5MAOfNX2tdQ254EsfRtsXfm0OS5RqirlbwCKlSM');
 
         return $this->render('TwitterBundle:Default:index.html.twig', array('name' => $name));
+    }
+
+    protected function testAPI($accessToken)
+    {
+        $requestHandler = new TwitterRequestHandler();
+        $requestOptions = [
+            'path' => '/1.1/statuses/user_timeline.json?count=10&screen_name=xedin',
+            'authorization' => 'Bearer ' . $accessToken,
+            'method' => 'GET'
+        ];
+
+        $responseReader = new TwitterResponseReader($requestHandler->request($requestOptions));
+        return $responseReader->getArrayResult();
     }
 }
