d1d6ba0639bd133e20ed4cfc18add4257c4f05e4
Polevoy Alexander <xedinaska@gmail.com>
2015-07-17 17:44:34 +0300

    Added first version of response reader (get response data / status / errors)

--- twitterAPP/src/Xedinaska/TwitterBundle/Service/TwitterResponseReader.php
+++ twitterAPP/src/Xedinaska/TwitterBundle/Service/TwitterResponseReader.php
@@ -5,9 +5,44 @@ namespace Xedinaska\TwitterBundle\Service;
 class TwitterResponseReader
 {
     protected $response;
+    protected $errors;
 
     public function __construct($responseString)
     {
         $this->response = json_decode($responseString);
+        $this->errors = isset($this->response->errors) ? $this->response->errors : [];
+    }
+
+    public function getArrayResult()
+    {
+        if ($this->isSuccess()) {
+            return $this->response;
+        }
+
+        return $this->errors;
+    }
+
+    public function get($field)
+    {
+        if ($this->isSuccess()) {
+            return property_exists($this->response, $field) ? $this->response->{$field} : null;
+        }
+
+        return null;
+    }
+
+    public function isSuccess()
+    {
+        return (count($this->errors) <= 0);
+    }
+
+    public function getCode()
+    {
+        return $this->errors[0]->code;
+    }
+
+    public function getMessage()
+    {
+        return $this->errors[0]->message;
     }
 }
